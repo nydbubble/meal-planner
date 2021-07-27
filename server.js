33cef4c6e2b5d98ccb62ac507c8d75cdf3c39178
nydbubble <nyd.bubble@protonmail.com>
2021-07-27 11:07:32 +0800

    Change port logs

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const connectDB = require("./config/db");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -24,4 +23,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(port, () => console.log("Server started"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
